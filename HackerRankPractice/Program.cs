33f0d0865001ac3068a4a911b0aa814d9e4efb01
Avinash Kumar <avinash.cse10@gmail.com>
2022-02-04 21:25:57 -0500

    Init

---
+++ HackerRankPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
